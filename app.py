4791c368ea6a4f4a549c50659b7ffd8d417f4054
mattmatt91 <matthias_muhr@web.de>
2022-02-15 01:59:04 +0100

    updating app

--- app.py
+++ app.py
@@ -4,117 +4,102 @@ from tkinter import messagebox
 import tkinter as tk
 from time import sleep
 from tkinter import filedialog
+from os import listdir
+from os.path import isfile, join
+import json as js
 
+def get_files(path):
+    print('test')
+    try:
+        onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+        print(onlyfiles)
+    except Exception as e:
+        print(e)
+
+
+def read_json():
+    with open('properties.json') as json_file:
+        return js.load(json_file)
 
 
 class App():
     
-    samplelist = [
-        "TATP",
-        "HMTD",
-        "TNT",
-        "Silverazide"
-        ] 
-    
     def __init__(self):
         # main window
         self.root = tk.Tk()
-        self.root.geometry("300x200")
-        
+        self.root.geometry("600x400")
+        margin = 0.23
+
+        #reading props
+        properties = read_json()
+        self.samplelist = [i for i in properties['samples']]
+        self.folder_path = StringVar()
+        self.folder_path.set(properties['path'])
+
+
+        # folde browser  
+        self.button_folder_browser = Button(text="Browse", command=self.browse_button)
+        self.button_folder_browser.pack()
+
         # labels
         self.text = tk.StringVar()
         self.text.set("analyzing data")
         self.label = tk.Label(self.root, textvariable=self.text)
         self.label.pack()
+
+
+        self.label_path = tk.Label(self.root, textvariable=self.folder_path)
+        self.label_path.pack()
         
+
+        # entrys
+        entry_heigth = tk.Entry(self.root)
+        entry_heigth.pack()
+
         # Buttons
         self.button_start = tk.Button(self.root,
-                                text="Click to change text below",
-                                command=self.changeText)
+                                text="start measurement",
+                                command=self.start_measurement)
         self.button_start.pack()
         self.button_start
         
         
         
         # check button
-        checkvar_read = BooleanVar()
-        self.check_read = Checkbutton(self.root, text = "read files", variable = checkvar_read, 
+        checkvar_spec = BooleanVar()
+        self.check_spec = Checkbutton(self.root, text = "log spectrometer", variable = checkvar_spec, 
                  onvalue = True, offvalue = False, height=1, 
                  width = 20)
-        self.check_read.pack()
+        self.check_spec.pack()
         
-        checkvar_statistics = BooleanVar()
-        self.check_statistics = Checkbutton(self.root, text = "do statistics", variable = checkvar_statistics, 
-                 onvalue = True, offvalue = False, height=1, 
-                 width = 20)
-        self.check_statistics.pack()
-        
-        checkvar_compare = BooleanVar()
-        self.check_compare = Checkbutton(self.root, text = "do statistics", variable = checkvar_compare, 
-                 onvalue = True, offvalue = False, height=1, 
-                 width = 20)
-        self.check_compare.pack()
         
         
         # dropdowns
         self.sample = tk.StringVar()
-        self.sample.set(App.samplelist[0])
-        self.dropdown_sample = tk.OptionMenu(self.root, self.sample, *App.samplelist)
+        self.sample.set(self.samplelist[0])
+        self.dropdown_sample = tk.OptionMenu(self.root, self.sample, *self.samplelist)
         self.dropdown_sample.config(width=90)
         self.dropdown_sample.pack()
         
+        
+
         # running main
         self.root.mainloop()
 
         
-    def changeText(self):
-        self.text.set("counting")
+    def create_properties():
+        pass
+
+
+    def start_measurement(self):
+        print('test')
+        get_files(self.folder_path.get())
         
-    def browseFiles(self):
-        self.filename = filedialog.askopenfilename(initialdir = "/",
-                                          title = "Select a File",
-                                          filetypes = (("Text files",
-                                                        "*.txt*"),
-                                                       ("all files",
-                                                        "*.*")))
-      
-        label_file_explorer.configure(text="File Opened: "+filename)
         
-               
+    def browse_button(self):
+        filename = filedialog.askdirectory()
+        self.folder_path.set(filename)      
 
 app =App()
 
-"""
-window.geometry("500x500")
-  
-#Set window background color
-window.config(background = "white")
-  
-# Create a File Explorer label
-label_file_explorer = Label(window,
-                            text = "File Explorer using Tkinter",
-                            width = 100, height = 4,
-                            fg = "blue")
-  
-      
-button_explore = Button(window,
-                        text = "Browse Files",
-                        command = browseFiles)
-  
-button_exit = Button(window,
-                     text = "Exit",
-                     command = exit)
-  
-# Grid method is chosen for placing
-# the widgets at respective positions
-# in a table like structure by
-# specifying rows and columns
-label_file_explorer.grid(column = 1, row = 1)
-  
-button_explore.grid(column = 1, row = 2)
-  
-button_exit.grid(column = 1,row = 3)
-  
-# Let the window wait for any events
-window.mainloop()
-"""
+
