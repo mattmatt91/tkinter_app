12ec6567b95861b58083dd17f41f071aa6bca73e
mattmatt91 <matthias_muhr@web.de>
2022-02-15 22:31:34 +0100

    update

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ import tkinter as tk
 from time import sleep
 from tkinter import filedialog
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 import json as js
 from datetime import datetime
 
 class Communication:
     def get_files(path):
-        print('test')
         try:
-            onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-            print(onlyfiles)
+            if isdir(path):
+                onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+            else:
+                print("path does not exist")
         except Exception as e:
             print(e)
 
@@ -22,6 +23,22 @@ class Communication:
     def read_json():
         with open('properties.json') as json_file:
             return js.load(json_file)
+
+
+    def update_properties(properties):
+        """
+        This function updates the properties.jsoen file.
+
+        Args:
+            properties (dictionary): dictionary with all parameters for the measurement
+        """
+        update_json = {}
+        for item in "rate duration sensors path lastdir droptime samples number".split():
+            update_json[item]= properties[item]
+
+        json_string = js.dumps(update_json, indent=4, sort_keys=True)
+        with open('properties.json', 'w') as outfile:
+            outfile.write(json_string)
 
 
 class App():
@@ -36,27 +53,13 @@ class App():
         self.col_2b = (self.width_window/4)*3 
         self.center = self.width_window/3 
         self.row_counter = 0
-        self.row_distance = 50  
+        self.row_distance = 50 
+
         self.goemetry = '{0}x{1}'.format(self.width_window, self.height_window)
-        print(self.goemetry)
         self.root = tk.Tk()
         self.root.geometry(self.goemetry)
-        margin = 0.23
+        self.reload_properties()
 
-        #reading props and setting varaibles
-        properties = Communication.read_json()
-        self.sensors = properties['sensors']
-        self.samplelist = [i for i in properties['samples']]
-        self.folder_path = StringVar()
-        self.folder_path.set(properties['path'])
-        self.number = IntVar()
-        self.height_window = IntVar()
-        self.droptime = IntVar()
-        self.duration = IntVar()
-        self.path = StringVar()
-        self.duration = IntVar()
-        self.sensors = properties['sensors']
-        
 
         # folde browser
         self.label_path_preview = tk.Label(self.root, text='path')
@@ -65,44 +68,40 @@ class App():
         self.button_folder_browser = Button(text="Browse", command=self.browse_button)
         self.button_folder_browser.place(x=self.col_1b, y=self.set_row(next=False))
         
-        self.label_path = tk.Label(self.root, textvariable=self.folder_path, wraplengt=150)
+        self.label_path = tk.Label(self.root, textvariable=self.path, wraplengt=150)
         self.label_path.place(x=self.col_2, y=self.set_row(next=False)) 
         
         
-
-
-
-    
         # entrys
         self.label_heigth = tk.Label(self.root, text='height')
         self.label_heigth.place(x=self.col_1, y=self.set_row())
         self.entry_heigth = tk.Entry(self.root)
+        self.entry_heigth.insert(0, "50")
         self.entry_heigth.place(x=self.col_2, y=self.set_row(next=False))
 
         self.label_number = tk.Label(self.root, text='number')
         self.label_number.place(x=self.col_1, y=self.set_row())
         self.entry_number = tk.Entry(self.root)
+        self.entry_number.insert(0, self.number.get())
         self.entry_number.place(x=self.col_2, y=self.set_row(next=False))
         
         self.label_droptime = tk.Label(self.root, text='droptime')
         self.label_droptime.place(x=self.col_1, y=self.set_row())
         self.entry_droptime = tk.Entry(self.root)
+        self.entry_droptime.insert(0, self.droptime.get())
         self.entry_droptime.place(x=self.col_2, y=self.set_row(next=False))
         
         self.label_duration = tk.Label(self.root, text='duration')
         self.label_duration.place(x=self.col_1, y=self.set_row())
         self.entry_duration = tk.Entry(self.root)
+        self.entry_duration.insert(0, self.duration.get())
         self.entry_duration.place(x=self.col_2, y=self.set_row(next=False))
         
         self.label_rate = tk.Label(self.root, text='rate')
         self.label_rate.place(x=self.col_1, y=self.set_row())
         self.entry_rate = tk.Entry(self.root)
+        self.entry_rate.insert(0, self.rate.get())
         self.entry_rate.place(x=self.col_2, y=self.set_row(next=False))
-
-        
-
-        
-        
         
         # check button
         checkvar_spec = BooleanVar()
@@ -110,27 +109,60 @@ class App():
                  onvalue = True, offvalue = False)
         self.check_spec.place(x=self.col_1, y=self.set_row())
         
-        
-        
+
         # dropdowns
         self.label_sample = tk.Label(self.root, text='sample')
         self.label_sample.place(x=self.col_1, y=self.set_row())
         self.sample = tk.StringVar()
         self.sample.set(self.samplelist[0])
         self.dropdown_sample = tk.OptionMenu(self.root, self.sample, *self.samplelist)
-        # self.dropdown_sample.config()
         self.dropdown_sample.place(x=self.col_2, y=self.set_row(next=False))
         
+
         # Buttons
         self.button_start = tk.Button(self.root,
                                 text="start measurement",
                                 command=self.start_measurement)
         self.button_start.place(x=self.center, y=self.set_row())
 
+
         # running main
         self.root.mainloop()
 
+
+    #reading props and setting varaibles
+    def reload_properties(self):
+        self.properties = Communication.read_json()
+        self.sensors = self.properties['sensors']
+        self.samplelist = [i for i in self.properties['samples']]
+        self.folder_path = StringVar()
+        self.folder_path.set(self.properties['path'])
+        self.number = IntVar()
+        new_number = self.properties['number'] + 1
+        self.number.set(new_number)
+        self.droptime = IntVar()
+        self.droptime.set(self.properties['droptime'])
+        self.duration = IntVar()
+        self.duration.set(self.properties['duration'])
+        self.last_dir =StringVar()
+        self.last_dir.set(self.properties['lastdir'])
+        self.path = StringVar()
+        self.path.set(join(self.folder_path.get(), self.last_dir.get()))
+        self.rate = StringVar()
+        self.rate.set(self.properties['rate'])
         
+
+    def update_entrys(self):
+        self.entry_number.delete(0, 'end')
+        self.entry_droptime.delete(0, 'end')
+        self.entry_duration.delete(0, 'end')
+        self.entry_rate.delete(0, 'end')
+        self.entry_number.insert(0, self.number.get())
+        self.entry_droptime.insert(0, self.droptime.get())
+        self.entry_duration.insert(0, self.duration.get())
+        self.entry_rate.insert(0, self.rate.get())
+        
+
     def set_row(self, next=True):
         if next:
             self.row_counter += self.row_distance 
@@ -145,11 +177,12 @@ class App():
         sensors =self.sensors
         this_path = 'test'
         droptime = self.check_if_int(self.entry_droptime.get())
-        sample = self.sample
+        sample = self.sample.get()
         number = self.check_if_int(self.entry_number.get())
         height = self.check_if_int(self.entry_heigth.get())
         info = 'test info'
         time_stamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        last_dir = self.path.get()[self.path.get().rfind('\\')+1:]
         
         properties = {
                     "rate": rate,
@@ -161,14 +194,18 @@ class App():
                     "height": height,
                     "sample_number": number,
                     "info": info,
-                    "datetime": time_stamp
+                    "datetime": time_stamp,
+                    "number": number,
+                    "lastdir": last_dir,
+                    "samples": self.samplelist
                     }
         for item in properties:
             if properties[item] == None:
                 return
-            
-        print(properties)
-        return properties
+            else:
+                Communication.update_properties(properties)
+                self.update_entrys()
+                return properties
 
     def check_if_int(self,value):
         try:
@@ -182,7 +219,9 @@ class App():
         this_properties = self.create_properties()
         if this_properties == None:
             return
-        Communication.get_files(self.folder_path.get())
+        else:
+            print('starting measurement\n', this_properties)
+        self.reload_properties()
         
         
     def browse_button(self):
---
+++ main.py
@@ -0,0 +1,274 @@
+"""
+This is the main mudule of the repository. It's main function starts a
+measurement and guides the user through it. Set up path for saving data before start
+"""
+
+from datetime import datetime
+import json as js
+import os
+from pathlib import Path
+
+from readdata import read_data 
+
+def get_immediate_subdirectories(a_dir):
+    """
+    This function creates a list with immediate subdirectories.
+
+    Args:
+        a_dir (string): path where subfolders are to be searched for
+
+    Returns (list): list with pathes of subdiretories
+    """
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]  
+
+def read_json(filename):
+    """
+    This function reads a json file and returns the content.
+    The properties file with all parameters for the measurement
+    is read here.
+
+    Args:
+        filename (stirng): name of the json file
+
+    Returns:
+        json_file (dictionary): dictionary with properties
+    """
+    with open(filename) as json_file:
+        return js.load(json_file)
+    
+def get_path(properties):
+    """
+    This function guides the user into selecting the path to save the measurements.
+
+    Args:
+        properties (dictionary): dictionary with all parameters for the measurement
+
+    Returns:
+        folder (string): folder for saving measurements
+        path (string): path for saving maeasurements
+    """
+    path = properties['path']
+    last_dir = properties['lastdir']
+    print('current directory: {0}'.format(path))
+    print('select path')
+    print('press:')
+    folders = get_immediate_subdirectories(path)
+    folders_dict = {1: last_dir}
+    print('1 for last dir: ', last_dir)
+    i = 2
+    for folder in folders:
+        folders_dict[i] = folder
+        
+        print(i, 'for ', folder)
+        i += 1   
+    print('press 0 for new folder') 
+    folder = None
+    while folder == None:
+        user_input = input()
+        try:
+            folder = folders_dict[int(user_input)]
+        except:
+            try:
+                if int(user_input) == 0:
+                    user_input = input('set foldername: ')
+                    directory = str(user_input)
+                    this_path = os.path.join(path, directory)
+                    Path(this_path).mkdir(parents=True, exist_ok=True)
+                    print("Directory '%s' created" %directory)
+                    folder = directory
+            except:
+                pass
+                print('folder out of range')
+    print(folder, ' selected')
+    return folder, path
+
+def select_sample(samples):
+    """
+    This function guides the user into selecting a sample for the measurement.
+
+    Args:
+        samples (list): list with all stored samples
+
+    Returns:
+        sample (string): sensor for measurement
+    """
+    print('select sample')
+    print('press:')
+    for sample in samples:
+        print(sample, 'for', samples[sample])
+    sample = None
+    while sample == None:
+        user_input = input()
+        try:
+            sample = samples[user_input]
+        except:
+            print('samlple out of range')
+    print(sample, ' selected')
+    return sample
+
+def select_height():
+    """
+    This function guides the user into selecting height for measurements.
+
+    Returns:
+        height (int):height of the measurement
+    """
+    print('select height')
+    print('enter int in cm:')
+    height = None
+    while height == None:
+        user_input = input()
+        try:
+            height = int(user_input)
+        except:
+            pass
+    print('height: ', height, 'cm')
+    return height
+
+def set_number():
+    """
+    This function guides the user into selecting a number for the measurement.
+
+    Returns:
+        sample_number (int): number for measurement
+    """
+    print('select number')
+    print('enter int:')
+    sample_number = None
+    while sample_number == None:
+        user_input = input()
+        try:
+            sample_number = int(user_input)
+        except:
+            print('not an integer')
+    print('number: ', sample_number)
+    return sample_number
+
+def add_info():
+    """
+    This function guides the user into adding an info for the measurement.
+
+    Returns:
+        info (string): additional info for measurement
+    """
+    user_input = input('add info')
+    return user_input
+
+def update_properties(properties):
+    """
+    This function updates the properties.jsoen file.
+
+    Args:
+        properties (dictionary): dictionary with all parameters for the measurement
+    """
+    update_json = {}
+    for item in "rate duration sensors path lastdir droptime number".split():
+        update_json[item]= properties[item]
+
+    json_string = js.dumps(update_json)
+    with open('properties.json', 'w') as outfile:
+        outfile.write(json_string)
+    
+def mkdir(properties, folder=None):
+    """
+    This file creates a new directory for a new measurent.
+    The name consists of the sample, the number and a time stamp.
+
+    Args:
+        properties (dictionary): dictionary with all parameters for the measurement
+        folder (string): name for new folder
+
+    Returns:
+        path (string): path to measurement folder
+        path_file (string): path to measurement file
+    """
+    name = properties['sample'] + '_' + str(properties['sample_number']) + '_' + str(properties['datetime'])
+    if folder != None:
+        path = os.path.join(properties['path'], folder, name)
+    else:
+        path = os.path.join(properties['path'], name)
+    Path(path).mkdir(parents=True, exist_ok=True)
+    path_file = os.path.join(path, name)
+    path_file = path_file + '.txt'
+    return path, path_file
+
+def save_properties_measurement(properties):
+    """
+    This function saves the measurement specific properties in a file called
+    **info.json** in the measurement folder.
+
+    Args:
+        properties (dictionary): dictionary with all parameters for the measurement
+    """
+    save_properties = properties
+    del save_properties['lastdir']
+    json_string = js.dumps(save_properties)
+    json_name = properties['path'] + '\\info.json'
+    with open(json_name, 'w') as outfile:
+        outfile.write(json_string)
+
+def start_measurement():
+    """
+    This is the main funciton of the module.
+    It guides the user through the measurement via console.
+    """
+    # reading sample list
+    samples = read_json('samples.json')
+
+    # reading properties
+    properties = read_json('properties.json')
+
+    # select path
+    folder, path = get_path(properties)
+    properties['lastdir'] = folder
+
+    #select sample
+    sample = select_sample(samples)
+    properties["sample"]= sample
+
+    #select height
+    height = select_height()
+    properties["height"]= height
+
+    #select number of sample
+    sample_number = set_number()
+    properties['sample_number'] = sample_number
+
+    #add info
+    info = add_info()
+    properties['info'] = info
+    
+    #setting timestamp
+    properties["datetime"]= datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+
+    # updating poperties global json
+    update_properties(properties)
+    
+    # make dir for measurement
+    path, path_file = mkdir(properties, folder)
+    properties['path'] = path
+
+    # saving properties for measurement
+    save_properties_measurement(properties)
+
+    # main measuring part
+    read_data(properties, path_file)
+
+def start_measurement_app(properties):
+    # updating poperties global json
+    update_properties(properties)
+    
+    # make dir for measurement
+    path, path_file = mkdir(properties)
+    properties['path'] = path
+
+    # saving properties for measurement
+    save_properties_measurement(properties)
+
+    # main measuring part
+    read_data(properties, path_file)
+
+
+if __name__ == '__main__':
+    start_measurement()
